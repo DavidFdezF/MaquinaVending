f3ea1529835836611f26db9f32ada33ad4f0ca00
Diego4621996 <diegoriverapoyo@gmail.com>
2024-04-13 11:41:57 +0200

    Clase Usuario

---
+++ Maquina Vending/Maquina Vending/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquina_Vending {
+    internal abstract class Usuario {
+
+        //Definición de los parámetros
+        public string Nombre {  get; set; }
+        public string Pin {  get; set; }
+
+        //COnstructores
+        public Usuario() { }
+
+        public Usuario(string nombre, string pin) {
+            Nombre = nombre;
+            Pin = pin;
+        }
+
+        //Métodos abstractos que van a implementar las clases hijas (Cliente y Admin)
+        public abstract void ComprarProductos();
+        public abstract void MostrarInformación();
+    }
+}
